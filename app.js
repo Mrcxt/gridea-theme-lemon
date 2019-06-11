6ce0c946387fba41923a874e5c94f79667873488
mrcxt <847428891@qq.com>
2019-06-11 23:21:39 +0800

    update v1.0.0

--- app.js
+++ app.js
@@ -13,41 +13,54 @@ app.set('view engine', 'ejs');
 /**
  * Home Page & Post List Page
  */
-app.get('/', async (req, res) => {
-  const response = await axios.get('https://raw.githubusercontent.com/getgridea/mock-json/master/list.json')
-  res.render('index', { ...response.data })
+app.get('/', async(req, res) => {
+    const response = await axios.get('https://raw.githubusercontent.com/getgridea/mock-json/master/list.json')
+    console.log({
+        ...response.data
+    });
+    res.render('index', {
+        ...response.data
+    })
 })
 
 /**
  * Post Page
  */
-app.get('/post/:postName', async (req, res) => {
-  const response = await axios.get('https://raw.githubusercontent.com/getgridea/mock-json/master/post.json')
-  res.render('post', { ...response.data })
+app.get('/post/:postName', async(req, res) => {
+    const response = await axios.get('https://raw.githubusercontent.com/getgridea/mock-json/master/post.json')
+    res.render('post', {
+        ...response.data
+    })
 })
 
 /**
  * Archives Page
  */
-app.get('/archives', async (req, res) => {
-  const response = await axios.get('https://raw.githubusercontent.com/getgridea/mock-json/master/archives.json')
-  res.render('archives', { ...response.data })
+app.get('/archives', async(req, res) => {
+    const response = await axios.get('https://raw.githubusercontent.com/getgridea/mock-json/master/archives.json')
+    res.render('archives', {
+        ...response.data
+    })
 })
 
 /**
  * tags Page
  */
-app.get('/tags', async (req, res) => {
-  const response = await axios.get('https://raw.githubusercontent.com/getgridea/mock-json/master/tags.json')
-  res.render('tags', { ...response.data })
+app.get('/tags', async(req, res) => {
+    const response = await axios.get('https://raw.githubusercontent.com/getgridea/mock-json/master/tags.json')
+    res.render('tags', {
+        ...response.data
+    })
 })
 
 /**
  * tag Page
  */
-app.get('/tag/:tagName', async (req, res) => {
-  const response = await axios.get('https://raw.githubusercontent.com/getgridea/mock-json/master/tag.json')
-  res.render('tag', { ...response.data })
+app.get('/tag/:tagName', async(req, res) => {
+    const response = await axios.get('https://raw.githubusercontent.com/getgridea/mock-json/master/tag.json')
+    res.render('tag', {
+        ...response.data
+    })
 })
 
 //使用8080端口
